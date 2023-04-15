464ddf704a3ee2d46bbc40220331b0bb27d4e767
Guilherme Gall <gmgall@gmail.com>
2023-04-15 02:54:34 -0300

    Adiciona Untappd

--- generate_feeds.sh
+++ generate_feeds.sh
@@ -8,3 +8,13 @@ newslinkrss \
         --follow \
         'https://gauchazh.clicrbs.com.br/pioneiro/ultimas-noticias/' \
         -o ./feeds/gauchazh.xml
+
+newslinkrss \
+        -p '.+/user/gmgall/checkin/[0-9]+' \
+        --follow \
+        --date-from-csss '.time' \
+        --title-regex 'Guilherme is drinking a (.+) on Untappd' \
+        --with-body \
+        --body-csss '.checkin-info.pad-it' \
+        'https://untappd.com/user/gmgall' \
+        -o ./feeds/gmgall_untappd.xml
