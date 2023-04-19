2d24c47435367935d73b537788146854f8b948b4
Guilherme Gall <gmgall@gmail.com>
2023-04-19 00:11:56 -0300

    Adiciona feeds para páginas no ctrl-c.club

--- generate_feeds.sh
+++ generate_feeds.sh
@@ -49,3 +49,137 @@ newslinkrss \
         --date-from-xpath '//@datetime' \
         'https://www.dannyguo.com/blog' \
         -o ./feeds/danny_guo.xml
+
+# Folks at ctrl-c.club
+
+newslinkrss \
+        -p '.*/~lettuce/.*' \
+        -a '([0-9]{4}-[0-9]{2}-[0-9]{2}) - .*' \
+        --text-date-fmt '%Y-%m-%d' \
+        --follow \
+        --require-dates \
+        --with-body \
+        --title '~lettuce posts on ctrl-c.club' \
+        --title-regex '/home/lettuce/public_html/(.*)' \
+        'https://ctrl-c.club/~lettuce/' \
+        -o ./feeds/lettuce.xml
+
+newslinkrss \
+         -p '.*/~FikaMedHasse/blogposts/.*' \
+         --follow \
+         --with-body \
+         --body-remove-csss 'header' \
+         --body-remove-csss 'footer' \
+         --title '~FikaMedHasse posts on ctrl-c.club' \
+         'https://ctrl-c.club/~FikaMedHasse/blog.html' \
+         -o ./feeds/FikaMedHasse.xml
+
+newslinkrss \
+         -p '.*/~gome/journal/.*' \
+         -i '.*/~gome/journal/rss/.*' \
+         -i '.*/~gome/journal/page*' \
+         --body-remove-csss '#header' \
+         'https://ctrl-c.club/~gome/journal/' \
+         --csss-date-regex '([0-9]{2} [A-z]{3} [0-9]{4}), .*' \
+         --date-from-csss 'time' \
+         --csss-date-fmt '%d %b %Y' \
+         --follow \
+         --with-body \
+         --title '~gome posts on ctrl-c.club' \
+         --encoding 'utf-8' \
+         -o ./feeds/gome.xml
+
+newslinkrss \
+         -p '.*/~chiptune/blog/.+' \
+         -i '.*/~chiptune/blog/feed.rss' \
+         --date-from-xpath '//@datetime' \
+         --xpath-date-fmt '%Y-%m-%dT00:00:00+00:00' \
+         --follow \
+         --with-body \
+         --title '~chiptune posts on ctrl-c.club' \
+         --title-regex '.+ - (.*)' \
+         --body-csss 'article' \
+         'https://ctrl-c.club/~chiptune/blog/' \
+         -o ./feeds/chiptune.xml
+
+newslinkrss \
+         -p '.+/~nodisc/[0-9]{4}-[0-9]{2}-[0-9]{2}_.+.html' \
+         --follow \
+         --date-from-text '([0-9]{4}-[0-9]{2}-[0-9]{2})[_ ].*' \
+         --text-date-fmt '%Y-%m-%d' \
+         --encoding 'utf-8' \
+         --with-body \
+         --title '"Artículos" section of ~nodisc' \
+         'https://ctrl-c.club/~nodisc/articulos.html' \
+         -o ./feeds/nodisc.xml
+
+newslinkrss \
+         -p '.*/~chickfilla/p/[0-9]{8}/.*' \
+         --date-from-url '.*/p/([0-9]{8})/.*' \
+         --url-date-fmt '%Y%m%d' \
+         --follow \
+         --with-body \
+         --title '~chickfilla posts on ctrl-c.club' \
+         --title-regex '[0-9]*: (.*)' \
+         'https://ctrl-c.club/~chickfilla/'  \
+         -o ./feeds/chickfilla.xml
+
+newslinkrss \
+         -p '.*/post[0-9]+.html' \
+         --follow \
+         --with-body \
+         --encoding 'utf-8' \
+         --title '~i00nsu posts on ctrl-c.club' \
+         'https://ctrl-c.club/~i00nsu/' \
+         -o ./feeds/i00nsu.xml
+
+newslinkrss \
+         -p '.+.html' \
+         -i '.+/index.html' \
+         --follow \
+         --with-body \
+         --title '~Triz3n posts on ctrl-c.club' \
+         --body-remove-csss 'nav' \
+         'https://ctrl-c.club/~Triz3n/' \
+         -o ./feeds/Triz3n.xml
+
+newslinkrss \
+         -p '.*/postits/.*.html' \
+         --follow \
+         --with-body \
+         'https://ctrl-c.club/~sicbail/' \
+         --title '~sicbail posts on ctrl-c.club' \
+         --encoding 'utf-8' \
+         -o ./feeds/sicbail.xml
+
+newslinkrss \
+         -p '.*.html' \
+         --title '~captaindrewboy posts on ctrl-c.club' \
+         --date-from-text '.+, (.*)' \
+         --text-date-fmt '%dth %B %Y' \
+         --follow \
+         --with-body \
+         'https://ctrl-c.club/~captaindrewboy/' \
+         -o ./feeds/captaindrewboy.xml
+
+newslinkrss \
+         -p '.+/~timemachine/[0-9]{4}/[0-9]{2}/[0-9]{2}/' \
+         --date-from-url '.+/~timemachine/([0-9]{4}/[0-9]{2}/[0-9]{2})/.*' \
+         --title '~timemachine posts on ctrl-c.club' \
+         --follow \
+         --with-body \
+         --body-csss 'article' \
+         'https://ctrl-c.club/~timemachine/archives/' \
+         -o ./feeds/timemachine.xml
+
+newslinkrss \
+         -p '.+/~beehiveth/entries/.+.html' \
+         --follow \
+         --with-body \
+         --body-csss 'article' \
+         --title '~beehiveth posts on ctrl-c.club' \
+         --date-from-url '.+/~beehiveth/entries/(.+).html' \
+         --url-date-fmt '%d%m%y' \
+         --encoding 'utf-8' \
+         'https://ctrl-c.club/~beehiveth/' \
+         -o ./feeds/beehiveth.xml
